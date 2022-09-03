0a0967b719b91c4a3fb4c664624c2e04acfc4346
chreden <4263940+chreden@users.noreply.github.com>
2022-09-03 14:40:16 +0100

    Start on framerate support
    #16

--- trashim/ShimDirect3D9.cpp
+++ trashim/ShimDirect3D9.cpp
@@ -2,6 +2,35 @@
 #include "ShimDirect3D9.h"
 #include "ShimDirect3DDevice9.h"
 #include "TRAWindowed.h"
+#include <unordered_map>
+
+namespace
+{
+    std::unordered_map<UINT, std::vector<D3DDISPLAYMODE>> adapter_display_modes;
+
+    std::vector<D3DDISPLAYMODE> store_display_modes(IDirect3D9& d3d9, UINT adapter, D3DFORMAT format)
+    {
+        auto found = adapter_display_modes.find(adapter);
+        if (adapter_display_modes.find(adapter) != adapter_display_modes.end())
+        {
+            return found->second;
+        }
+
+        std::vector<D3DDISPLAYMODE> modes;
+        uint32_t actual_count = d3d9.GetAdapterModeCount(adapter, format);
+        for (uint32_t i = 0; i < actual_count; ++i)
+        {
+            D3DDISPLAYMODE mode {};
+            d3d9.EnumAdapterModes(adapter, format, i, &mode);
+            modes.push_back(mode);
+        }
+
+        // TODO: Find the missing framerates - we want to provide 20-200
+        // TODO: Add the missing framerates.
+
+        adapter_display_modes.insert({ adapter, modes });
+    }
+}
 
 HRESULT ShimDirect3D9::CreateDevice(THIS_ UINT Adapter, D3DDEVTYPE DeviceType, HWND hFocusWindow, DWORD BehaviorFlags, D3DPRESENT_PARAMETERS* pPresentationParameters, IDirect3DDevice9** ppReturnedDeviceInterface)
 {
@@ -23,3 +52,15 @@ HRESULT ShimDirect3D9::CreateDevice(THIS_ UINT Adapter, D3DDEVTYPE DeviceType, H
     return hr;
 }
 
+UINT ShimDirect3D9::GetAdapterModeCount(UINT Adapter, D3DFORMAT Format)
+{
+    auto modes = store_display_modes(*_d3d9, Adapter, Format);
+    return modes.size();
+}
+
+HRESULT ShimDirect3D9::EnumAdapterModes(UINT Adapter, D3DFORMAT Format, UINT Mode, D3DDISPLAYMODE* pMode)
+{
+    auto modes = store_display_modes(*_d3d9, Adapter, Format);
+    *pMode = modes[Mode];
+    return D3D_OK;
+}
